9ccb902bc4c7fb7128139ba8f771c88c4cb65f99
FreyVik <yonkigamer96@gmail.com>
2022-10-14 12:00:02 +0200

    Form created

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
+  providers: [ProjectService]
 })
 export class CreateComponent implements OnInit {
+  public title: string;
+  public project: Project;
 
-  constructor() { }
+  constructor(
+    private _projectService: ProjectService
+  ){
+    this.title = "Crear proyecto";
+    this.project = new Project('','','','', 2022,'','');
+
+    /*
+    public _id: string,
+    public name: string,
+    public description: string,
+    public category: string,
+    public year: number,
+    public langs: string,
+    public image: string
+    */
+    
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(_form: any){
+    console.log(this.project);
+  }
 }
---
+++ src/app/services/global.ts
@@ -0,0 +1,4 @@
+export var Global = {
+    url: 'http://localhost:3700/',
+    urlProject: 'http://localhost:3700/project/'
+};
